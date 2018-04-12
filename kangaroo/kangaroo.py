5534c7455d602376bb061388db7819197daa152f
Jacob-Vlijm <vlijm@planet.nl>
2018-04-12 07:45:38 +0200

    Removed leading - from license info. No clue how it got there.

--- kangaroo/kangaroo.py
+++ kangaroo/kangaroo.py
@@ -7,19 +7,19 @@ from gi.repository import Budgie, GObject, Gtk
 
 
 """
--Kangaroo
--Author: Jacob Vlijm
--Copyright © 2017-2018 Ubuntu Budgie Developers
--Website=https://ubuntubudgie.org
--This program is free software: you can redistribute it and/or modify it under
--the terms of the GNU General Public License as published by the Free Software
--Foundation, either version 3 of the License, or any later version. This
--program is distributed in the hope that it will be useful, but WITHOUT ANY
--WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
--A PARTICULAR PURPOSE. See the GNU General Public License for more details. You
--should have received a copy of the GNU General Public License along with this
--program.  If not, see <https://www.gnu.org/licenses/>.
--"""
+Kangaroo
+Author: Jacob Vlijm
+Copyright © 2017-2018 Ubuntu Budgie Developers
+Website=https://ubuntubudgie.org
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version. This
+program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+should have received a copy of the GNU General Public License along with this
+program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 
 userhome = os.environ["HOME"]
