b0d70cb7167e4797be1484cc2b9019f4b47e0fa6
Jacob-Vlijm <vlijm@planet.nl>
2018-05-27 08:11:36 +0200

    Added README, illustrative images, adde license info.

---
+++ weathershow/README.md
@@ -0,0 +1,12 @@
+# WeatherShow
+
+WeatherShow is an applet, showing the curren weather on the desktop. The forecast is available in the applet's menu, available from the panel.
+
+Settings include position, textcolor, background transparency and language.
+
+![screenshot](https://github.com/UbuntuBudgie/experimental/blob/master/weathershow/screenshot1.png)
+
+![screenshot](https://github.com/UbuntuBudgie/experimental/blob/master/weathershow/screenshot2.png)
+
+![screenshot](https://github.com/UbuntuBudgie/experimental/blob/master/weathershow/screenshot3.png)
+
--- weathershow/getweather.py
+++ weathershow/getweather.py
@@ -3,6 +3,22 @@ import requests
 import json
 
 
+"""
+Budgie WeatherShow
+Author: Jacob Vlijm
+Copyright=Copyright © 2017-2018 Ubuntu Budgie Developers
+Website=https://ubuntubudgie.org
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version. This
+program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+should have received a copy of the GNU General Public License along with this
+program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
 def get_data(key, city, wtype, lang):
     # data = sky, temp, wind_dir, wind_speed, pressure
     lang = "&lang=" + lang if lang else ""
