dcb45e3a6d0a7af935f7595fe61defe7230de034
Jacob-Vlijm <vlijm@planet.nl>
2018-12-01 19:37:15 +0100

    Added license info.

--- development_only/tools/getcities.py
+++ development_only/tools/getcities.py
@@ -5,6 +5,21 @@ import requests
 import time
 
 """
+Author: Jacob Vlijm
+Copyright ©2018 Ubuntu Budgie Developers
+Website=https://ubuntubudgie.org
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or any later version. This
+program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+A PARTICULAR PURPOSE. See the GNU General Public License for more details. You
+should have received a copy of the GNU General Public License along with this
+program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+
+"""
 This script is to extract citydata, including state info, from:
 current.city.list.json.gz, as downloaded from
 http://bulk.openweathermap.org/sample/
